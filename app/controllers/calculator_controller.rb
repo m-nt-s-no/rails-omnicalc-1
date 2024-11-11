97c717e14a51641da84bd467aa04464a719c9b7b
Max Montesino <45675504+m-nt-s-no@users.noreply.github.com>
2024-11-11 22:51:08 +0000

    fixed routing bug for square and home page

--- app/controllers/calculator_controller.rb
+++ app/controllers/calculator_controller.rb
@@ -1,20 +1,20 @@
 class CalculatorController < ApplicationController
   def home
-    render({:template => "pages_templates/square_new"})
+    render({:template => "/square_new"})
   end
 
   def square
     @action = "Square"
     @input = params.fetch("number").to_f
     @result = @input ** 2
-    @back_link = "/pages_templates/square_new"
+    @back_link = "/square_new"
     @back_link_text = "Calculate another square"
     @data = {:input => @input, :result => @result}
     puts @data
-    render({:template => "pages_templates/results"})
+    render({:template => "/results"})
   end
 
   def results
-    render({:template => "pages_templates/results"})
+    render({:template => "/results"})
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   get("/", {:controller => "calculator", :action => "home"})
-  get("pages_templates/square_new", {:controller => "calculator", :action => "square"})
-  get("pages_templates/results", {:controller => "calculator", :action => "square"})
+  get("/square_new", {:controller => "calculator", :action => "home"})
+  get("/results", {:controller => "calculator", :action => "square"})
 end
