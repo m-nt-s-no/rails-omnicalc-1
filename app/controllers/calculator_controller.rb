2c56d1f4ce06b441717ac16a9542ae32747e587d
Max Montesino <45675504+m-nt-s-no@users.noreply.github.com>
2024-11-12 00:02:07 +0000

    added RCAV for square root

--- app/controllers/calculator_controller.rb
+++ app/controllers/calculator_controller.rb
@@ -1,20 +1,32 @@
 class CalculatorController < ApplicationController
   def home
-    render({:template => "/square_new"})
+    render({:template => "/square_form"})
+  end
+
+  def square_form
+    render({:template => "/square_form"})
   end
 
   def square
     @action = "Square"
     @input = params.fetch("number").to_f
     @result = @input ** 2
-    @back_link = "/square_new"
+    @back_link = "/square_form"
     @back_link_text = "Calculate another square"
-    @data = {:input => @input, :result => @result}
-    puts @data
-    render({:template => "/results"})
+    render({:template => "/square_results"})
   end
 
-  def results
-    render({:template => "/results"})
+  def sqrt_form
+    render({:template => "/sqrt_form"})
+  end
+
+  def square_root
+    @action = "Square Root"
+    @input = params.fetch("number").to_f
+    @result = Math.sqrt(@input)
+    @back_link = "/sqrt_form"
+    @back_link_text = "Calculate another square root"
+    @data = {:input => @input, :result => @result}
+    render({:template => "/sqrt_results"})
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   get("/", {:controller => "calculator", :action => "home"})
-  get("/square_new", {:controller => "calculator", :action => "home"})
-  get("/results", {:controller => "calculator", :action => "square"})
+  get("square_form", {:controller => "calculator", :action => "square_form"})
+  get("/square_results", {:controller => "calculator", :action => "square"})
+  get("/sqrt_form", {:controller => "calculator", :action => "sqrt_form"})
+  get("/sqrt_results", {:controller => "calculator", :action => "square_root"})
 end
